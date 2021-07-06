96f507f4080da6916fd395dc06a916c91aef8bec
kim-su-hwan <64351122+kim-su-hwan@users.noreply.github.com>
2021-07-06 14:38:38 +0900

    0706plus jumpfucntion

--- Assets/00KIM/Scipts/PlayerMove.cs
+++ Assets/00KIM/Scipts/PlayerMove.cs
@@ -7,6 +7,9 @@ public class PlayerMove : MonoBehaviour
     // Start is called before the first frame update
 
     public float movespeed = 3.0f;
+    public float turning_speed = 5.0f;
+
+    public float reverse_gravity = 5.0f;
 
     void Start()
     {
@@ -19,13 +22,29 @@ public class PlayerMove : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
 
+        float jum = Input.GetAxis("Jump");
+
+
+        float turning_X = Input.GetAxis("Mouse X");
+        float turning_Y = Input.GetAxis("Mouse Y");
+
+
         Vector3 dir = new Vector3(h, 0, v);
 
         float dis = dir.magnitude;
-
         dir.Normalize();
 
-        transform.position += dir * movespeed * Time.deltaTime;
+        Vector3 uping = new Vector3(0, jum, 0);
 
+        uping.Normalize();
+
+
+        transform.position += dir * movespeed * Time.deltaTime;
+        transform.position += uping * reverse_gravity * Time.deltaTime;
+
+
+
+        //transform.Rotate(Vector3.up * -turning_speed * turning_X);
+        //transform.Rotate(Vector3.right * turning_speed * turning_Y);
     }
 }
